b1e3a8b2d6544796a9e260aa0da48c6a57a3a023
linh-KAK <114237304+linh-KAK@users.noreply.github.com>
2022-12-27 17:53:26 -0500

    Cleaned up code, added test cases

--- Main.java
+++ Main.java
@@ -3,198 +3,127 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner in = new Scanner(System.in);
-    static String[] temp_column = new String[3];
-    static String[][] temp_face = new String[3][3];
 
     public static void main(String[] args) {
 
        CubeFace f1 = new CubeFace("R");
 
-       f1.blocks[0][0][0] = "Y";
-       f1.blocks[1][0][0] = "Y";
-       f1.blocks[2][0][0] = "B";
-
-       f1.blocks[0][1][0] = "R";
-       f1.blocks[1][1][0] = "B";
-       f1.blocks[2][1][0] = "O";
-
-       f1.blocks[0][2][0] = "R";
-       f1.blocks[1][2][0] = "B";
-       f1.blocks[2][2][0] = "G";
-
        CubeFace f2 = new CubeFace("W");
-
-        f2.blocks[0][0][0] = "O";
-        f2.blocks[1][0][0] = "R";
-        f2.blocks[2][0][0] = "B";
-
-        f2.blocks[0][1][0] = "G";
-        f2.blocks[1][1][0] = "R";
-        f2.blocks[2][1][0] = "B";
-
-        f2.blocks[0][2][0] = "Y";
-        f2.blocks[1][2][0] = "Y";
-        f2.blocks[2][2][0] = "G";
 
        CubeFace f3 = new CubeFace("O");
 
-        f3.blocks[0][2][0] = "W";
-        f3.blocks[1][2][0] = "R";
-        f3.blocks[2][2][0] = "R";
-
-        f3.blocks[0][1][0] = "R";
-        f3.blocks[1][1][0] = "R";
-        f3.blocks[2][1][0] = "W";
-
-        f3.blocks[0][0][0] = "O";
-        f3.blocks[1][0][0] = "G";
-        f3.blocks[2][0][0] = "R";
-
        CubeFace f4 = new CubeFace("B");
-
-        f4.blocks[0][0][0] = "G";
-        f4.blocks[1][0][0] = "W";
-        f4.blocks[2][0][0] = "Y";
-
-        f4.blocks[0][1][0] = "Y";
-        f4.blocks[1][1][0] = "R";
-        f4.blocks[2][1][0] = "B";
-
-        f4.blocks[0][2][0] = "O";
-        f4.blocks[1][2][0] = "W";
-        f4.blocks[2][2][0] = "W";
 
        CubeFace f5 = new CubeFace("G");
 
-        f5.blocks[0][0][0] = "R";
-        f5.blocks[1][0][0] = "B";
-        f5.blocks[2][0][0] = "B";
-
-        f5.blocks[0][1][0] = "W";
-        f5.blocks[1][1][0] = "G";
-        f5.blocks[2][1][0] = "Y";
-
-        f5.blocks[0][2][0] = "G";
-        f5.blocks[1][2][0] = "O";
-        f5.blocks[2][2][0] = "O";
-
        CubeFace f6 = new CubeFace("Y");
 
-        f6.blocks[0][0][0] = "O";
-        f6.blocks[1][0][0] = "G";
-        f6.blocks[2][0][0] = "W";
+        Rotate3Block r3 = new Rotate3Block();
 
-        f6.blocks[0][1][0] = "O";
-        f6.blocks[1][1][0] = "G";
-        f6.blocks[2][1][0] = "Y";
-
-        f6.blocks[0][2][0] = "G";
-        f6.blocks[1][2][0] = "G";
-        f6.blocks[2][2][0] = "Y";
-
-        chooseTurn(f1, f5, f6, f3, f4, f2);
+        chooseTurn(r3, f1, f5, f6, f3, f4, f2);
         printFaces(f1, f5, f6, f3, f2, f4);
     }
 
-    public static void chooseTurn(CubeFace f1, CubeFace f5, CubeFace f6, CubeFace f3, CubeFace f4, CubeFace f2) {
+    public static void chooseTurn(Rotate3Block r, CubeFace f1, CubeFace f5, CubeFace f6, CubeFace f3, CubeFace f4, CubeFace f2) {
         System.out.println("What turn? ");
         String turn = in.nextLine();
 
         if (turn.equals("R") || turn.equals("RP") || turn.equals("L") || turn.equals("LP") || turn.equals("M") || turn.equals("MP")) {
             switch (turn) {
                 case "R":
-                    turnColumn(f1, f5, f6, f3, 2, 0);//turn R
-                    turnFace(f2, 1);
+                    r.turnColumn(f1, f5, f6, f3, 2, 0);//turn R
+                    r.turnFace(f2, 1);
                     break;
                 case "RP":
-                    turnColumn(f1, f6, f5, f3, 2, 0); //turn R'
-                    turnFace(f2, 3);
+                    r.turnColumn(f1, f6, f5, f3, 2, 0); //turn R'
+                    r.turnFace(f2, 3);
                     break;
                 case "L":
-                    turnColumn(f1, f6, f5, f3, 0, 2); //turn L
-                    turnFace(f4, 1);
+                    r.turnColumn(f1, f6, f5, f3, 0, 2); //turn L
+                    r.turnFace(f4, 1);
                     break;
                 case "LP":
-                    turnColumn(f1, f5, f6, f3, 0, 2); //turn L'
-                    turnFace(f4, 3);
+                    r.turnColumn(f1, f5, f6, f3, 0, 2); //turn L'
+                    r.turnFace(f4, 3);
                     break;
                 case "M":
-                    turnColumn(f1, f6, f5, f3, 1, 1); //turn M
+                    r.turnColumn(f1, f6, f5, f3, 1, 1); //turn M
                     break;
                 case "MP":
-                    turnColumn(f1, f5, f6, f3, 1, 1); //turn M'
+                    r.turnColumn(f1, f5, f6, f3, 1, 1); //turn M'
                     break;
             }
 
         } else if (turn.equals("U") || turn.equals("UP") || turn.equals("D") || turn.equals("DP") || turn.equals("E") || turn.equals("EP")) {
-            turnFace(f1, 1);
-            turnFace(f4, 1);
-            turnFace(f2, 1);
-            turnFace(f3, 3);
+
+            r.turnFace(f1, 1);
+            r.turnFace(f4, 1);
+            r.turnFace(f2, 1);
+            r.turnFace(f3, 3);
 
             switch (turn) {
                 case "U":
-                    turnColumn(f1, f4, f2, f3, 2, 0);//turn U
-                    turnFace(f5, 1);
+                    r.turnColumn(f1, f4, f2, f3, 2, 0);//turn U
+                    r.turnFace(f5, 1);
                     break;
                 case "UP":
-                    turnColumn(f1, f2, f4, f3, 2, 0); //turn U'
-                    turnFace(f5, 3);
+                    r.turnColumn(f1, f2, f4, f3, 2, 0); //turn U'
+                    r.turnFace(f5, 3);
                     break;
                 case "D":
-                    turnColumn(f1, f2, f4, f3, 0, 2); //turn D
-                    turnFace(f6, 1);
+                    r.turnColumn(f1, f2, f4, f3, 0, 2); //turn D
+                    r.turnFace(f6, 1);
                     break;
                 case "DP":
-                    turnColumn(f1, f4, f2, f3, 0, 2); //turn D'
-                    turnFace(f6, 3);
+                    r.turnColumn(f1, f4, f2, f3, 0, 2); //turn D'
+                    r.turnFace(f6, 3);
                     break;
                 case "E":
-                    turnColumn(f1, f2, f4, f3, 1, 1); //turn E
+                    r.turnColumn(f1, f2, f4, f3, 1, 1); //turn E
                     break;
                 case "EP":
-                    turnColumn(f1, f4, f2, f3, 1, 1); //turn E'
+                    r.turnColumn(f1, f4, f2, f3, 1, 1); //turn E'
                     break;
             }
 
-            turnFace(f1, 3);
-            turnFace(f4, 3);
-            turnFace(f2, 3);
-            turnFace(f3, 1);
+            r.turnFace(f1, 3);
+            r.turnFace(f4, 3);
+            r.turnFace(f2, 3);
+            r.turnFace(f3, 1);
 
         } else if (turn.equals("B") || turn.equals("BP") || turn.equals("F") || turn.equals("FP") || turn.equals("S") || turn.equals("SP")) {
 
-            turnFace(f5, 1); // turns entire face clockwise
-            turnFace(f6, 3); // turns entire face counter-clockwise
+            r.turnFace(f5, 1); // turns entire face clockwise
+            r.turnFace(f6, 3); // turns entire face counter-clockwise
 
             switch (turn) {
                 case "B":
-                    turnColumn(f2, f5, f6, f4, 2, 0); // turn B
-                    turnFace(f3, 1);
+                    r.turnColumn(f2, f5, f6, f4, 2, 0); // turn B
+                    r.turnFace(f3, 1);
                     break;
                 case "BP":
-                    turnColumn(f2, f6, f5, f4, 2, 0); // turn B'
-                    turnFace(f3, 3);
+                    r.turnColumn(f2, f6, f5, f4, 2, 0); // turn B'
+                    r.turnFace(f3, 3);
                     break;
                 case "F":
-                    turnColumn(f2, f6, f5, f4, 0, 2); // turn F
-                    turnFace(f1, 1);
+                    r.turnColumn(f2, f6, f5, f4, 0, 2); // turn F
+                    r.turnFace(f1, 1);
                     break;
                 case "FP":
-                    turnColumn(f2, f5, f6, f4, 0, 2); // turn F'
-                    turnFace(f1, 3);
+                    r.turnColumn(f2, f5, f6, f4, 0, 2); // turn F'
+                    r.turnFace(f1, 3);
                     break;
                 case "S":
-                    turnColumn(f2, f6, f5, f3, 1, 1); // turn S
+                    r.turnColumn(f2, f6, f5, f3, 1, 1); // turn S
                     break;
                 case "SP":
-                    turnColumn(f2, f5, f6, f3, 1, 1); // turn S'
+                    r.turnColumn(f2, f5, f6, f3, 1, 1); // turn S'
                     break;
 
             }
 
-            turnFace(f5, 3);
-            turnFace(f6, 1);
+            r.turnFace(f5, 3);
+            r.turnFace(f6, 1);
         }
 
     }
@@ -222,40 +151,5 @@ public class Main {
             System.out.println();
         }
     }
-
-
-    public static void turnColumn(CubeFace f1, CubeFace f5, CubeFace f6, CubeFace f3, int i1, int i2) {
-
-        for (int i = 0; i < 3; i++) {
-            temp_column[i] = f5.blocks[i][i1][0];
-            f5.blocks[i][i1][0] = f1.blocks[i][i1][0];
-            f1.blocks[i][i1][0] = f6.blocks[i][i1][0];
-            f6.blocks[i][i1][0] = f3.blocks[~(i - 1) + 2][i2][0];
-        }
-
-        for (int i = 0; i < 3; i++) {
-            f3.blocks[i][i2][0] = temp_column[~(i - 1) + 2];
-        }
-    }
-
-    public static void turnFace(CubeFace f2, int turns) {
-        for (int n = 0; n < turns; n++) {
-
-            //temporarily stores the new orientation of columns and rows
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    temp_face[i][j] = f2.blocks[~(j - 1) + 2][i][0];
-                }
-            }
-
-            //updates the face with the correct and original orientation
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    f2.blocks[i][j][0] = temp_face[i][j];
-                }
-            }
-        }
-    }
-
 
 }
--- Relations.java
+++ Relations.java
@@ -1,30 +1,3 @@
 public class Relations {
 
-//    int[] R = {f1, f5, f6, f3, 2, 0)};
-//    int[] RP = {f1, f6, f5, f3, 2, 0};
-//    int[] L = {f1, f6, f5, f3, 0, 2};
-//    int[] LP = {f1, f5, f6, f3, 0, 2};
-//    int[] M = {f1, f5, f6, f3;
-//    int[]  = {f1, f6, f5, f3};
-
-    //turnL(f1, f6, f5, f3);
-//    public static void turnL(CubeFace f1, CubeFace f5, CubeFace f6, CubeFace f3) {
-//
-//        for (int i = 0; i < 3; i++) {
-//            temp_column[i] = f6.blocks[i][0][0];
-//        }
-//
-//        for (int i = 0; i < 3; i++) {
-//            f6.blocks[i][0][0] = f1.blocks[i][0][0];
-//            f1.blocks[i][0][0] = f5.blocks[i][0][0];
-//            f5.blocks[i][0][0] = f3.blocks[~(i - 1) + 2][2][0];
-//            f3.blocks[i][2][0] = temp_column[~(i - 1) + 2];
-//        }
-//    }
-
-    //        printFaces(f1, f5, f6, f3);
-//        turnR(f1, f5, f6, f3);
-//        printFaces(f1, f5, f6, f3);
-//        turnR(f1, f6, f5, f3);
-
 }
