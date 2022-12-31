291613a62b39526de599e88222c4c879a44f0970
linh-KAK <114237304+linh-KAK@users.noreply.github.com>
2022-12-30 19:29:21 -0500

    Edited structure of Solution file

--- Solution.java
+++ Solution.java
@@ -1,6 +1,23 @@
 public class Solution {
 
-    public static void checkWhite(CubeFace f1, CubeFace f2, CubeFace f3, CubeFace f4) {
+    static CubeFace f1;
+    static CubeFace f2;
+    static CubeFace f3;
+    static CubeFace f4;
+    static CubeFace f6;
+
+    Solution (CubeFace f1, CubeFace f2, CubeFace f3, CubeFace f4, CubeFace f6) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.f3 = f3;
+        this.f4 = f4;
+        this.f6 = f6;
+    }
+
+    public void checkWhiteBottom() {
+    }
+
+    public static void checkWhite() {
 
         CubeFace[] faces = {f1, f2, f3, f4};
 
@@ -8,16 +25,25 @@ public class Solution {
         for (int i = 0; i < 1; i++) {
 
             if ((faces[i]).blocks[1][0] ==  "W") {
-                moveToTop("f" + (i+1), 0);
+                moveEdgeToBottom("f" + (i+1), 0);
             }
 
             if ((faces[i]).blocks[1][2] ==  "W") {
-                moveToTop("f" + (i+1), 2);
+                moveEdgeToBottom("f" + (i+1), 2);
             }
+
+            if ((faces[i]).blocks[0][1] ==  "W") {
+                moveMidToBottom("f" + (i+1), "");
+            }
+
+            if ((faces[i]).blocks[2][1] ==  "W") {
+                moveMidToBottom("f" + (i+1), "P");
+            }
+
         }
     }
 
-    public static void moveToTop(String face, int column) {
+    public static void moveEdgeToBottom(String face, int column) {
 
         switch (column) {
             case 0:
@@ -37,6 +63,7 @@ public class Solution {
                         break;
 
                 }
+
             case 2:
 
                 switch (face) {
@@ -55,8 +82,51 @@ public class Solution {
                         break;
                 }
 
+
         }
 
     }
+
+    public static void moveMidToBottom(String face, String isPrime) {
+
+        // if isPrime = P then the white block is in last row position; if isPrime is empty then the white block is in top row position
+
+        switch (face) {
+            case "f1":
+                Main.chooseTurn("F" + isPrime); //isPrime checks if the white block is in the top or bottom position
+                Main.chooseTurn("RP");
+                Main.chooseTurn("DP");
+                Main.chooseTurn("R");
+                break;
+            case "f2":
+                Main.chooseTurn("R" + isPrime);
+                Main.chooseTurn("BP");
+                Main.chooseTurn("DP");
+                Main.chooseTurn("B");
+                break;
+            case "f3":
+                Main.chooseTurn("B" + isPrime);
+                Main.chooseTurn("LP");
+                Main.chooseTurn("DP");
+                Main.chooseTurn("L");
+                break;
+            case "f4":
+                Main.chooseTurn("L" + isPrime);
+                Main.chooseTurn("FP");
+                Main.chooseTurn("DP");
+                Main.chooseTurn("F");
+                break;
+        }
+
+    }
+
+    public static void findTopAdjColor() {
+
+    }
+
+    public static void rotateColorMatch() {
+
+    }
+
 
 }
